225e4b54d717ed141d9d654ad42207ebb1381c2d
Maizify <lariagle@gmail.com>
2016-12-28 16:20:25 +0800

    update vConsole, add WeChat plugin

--- src/config.js
+++ src/config.js
@@ -1,1 +1,22 @@
-vConsole.removePlugin('network');
+(function() {
+  vConsole.removePlugin('network');
+
+  var wx = {version: {version: 0, updateTime: 'xxxx.xx.xx'}}
+  if (!!wx && !!wx.version) {
+    console.info('[system]', 'WeChatLib:', wx.version.version + ' (' + wx.version.updateTime + ')');
+  }
+  
+  var wxVConsoleTab = new vConsole.VConsolePlugin('wechat', 'WeChat');
+  wxVConsoleTab.on('init', function() {
+
+  });
+  wxVConsoleTab.on('renderTab', function(callback) {
+    var html = [
+      '<div style="padding:6px 8px;border-bottom:1px solid #EEE">', // wx.clearStorage()
+        '<a style="border:1px solid #D9D9D9; background:#efeff4; display:inline-block; padding:6px 8px; color:#000;" href="javascript:;" onclick="wx.clearStorage();">wx.clearStorage()</a>',
+      '</div>'
+    ];
+    callback(html.join(''));
+  });
+  vConsole.addPlugin(wxVConsoleTab);
+})();
